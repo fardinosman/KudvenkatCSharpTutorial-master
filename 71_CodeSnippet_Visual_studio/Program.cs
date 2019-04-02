45a476362ef8e437d3e745e62091c7b7e8a330b8
Fardin O <36036540+fardinosman@users.noreply.github.com>
2019-04-02 05:59:09 +0200

    _71_CodeSnippet_Visual_studio

---
+++ 71_CodeSnippet_Visual_studio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _71_CodeSnippet_Visual_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < length; i++)
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+    }
+}
+ /*
+  
+     ctrk k x*/
